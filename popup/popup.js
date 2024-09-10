fbff252e3d255b59adf9733658af188abaa51b54
Marcelo Maciel <marselo.maciel@gmail.comm>
2024-09-10 00:08:53 -0300

    conclui gerenciamento de tarefas e inclui um pouco de estilização

--- popup/popup.js
+++ popup/popup.js
@@ -1,29 +1,17 @@
-const items = [{ "item": "Record next video", "status": 0 },
-{ "item": "Record another video", "status": 1 },]
-
-const itemsStr = JSON.stringify(items);
-
-console.log(items)
-console.log(itemsStr)
-
-const recurringItems = [{ "item": "Record next video" },
-    { "item": "Record another video" },]
-    
-    const recurringItemsStr = JSON.stringify(recurringItems);
-    
-    console.log(recurringItems)
-    console.log(recurringItemsStr)
+var editingTaskIndex = 0;
+var isClosed = true;
 
 document.querySelector('.create-task').addEventListener('click', function () {
     document.querySelector('.new-task').style.display = 'block';
+    document.querySelector('.edit-daily-task-wrapper').style.display = 'none';
+    document.querySelector('.create-task').style.display = 'none';
 })
 
 document.querySelector('.new-task button').addEventListener('click', function () {
     var itemName = document.querySelector('.new-task input').value;
 
-    if (itemName != '') {
-
-        var itemsStorage = localStorage.getItem('todo-items');
+    if (itemName.trim() != '') {
+        var itemsStorage = localStorage.getItem(getCurrentDateTime());
         var itemsArr = JSON.parse(itemsStorage);
 
         itemsArr.push({ "item": itemName, "status": 0 });
@@ -32,6 +20,24 @@ document.querySelector('.new-task button').addEventListener('click', function ()
         fetchItems();
         document.querySelector('.new-task input').value = '';
         document.querySelector('.new-task').style.display = 'none';
+        document.querySelector('.create-task').style.display = 'block';
+    }
+})
+
+document.querySelector('.edit-daily-task button').addEventListener('click', function () {
+    var itemName = document.querySelector('.edit-daily-task input').value;
+
+    if (itemName.trim() != '') {
+        var itemsStorage = localStorage.getItem(getCurrentDateTime());
+        var itemsArr = JSON.parse(itemsStorage);
+
+        itemsArr[editingTaskIndex].item = itemName;
+
+        saveItems(itemsArr);
+        fetchItems();
+        document.querySelector('.edit-daily-task input').value = '';
+        document.querySelector('.edit-daily-task-wrapper').style.display = 'none';
+        document.querySelector('.create-task').style.display = 'block';
     }
 })
 
@@ -42,22 +48,34 @@ function fetchItems() {
     var newItemHtml = '';
 
     try {
-        var items = localStorage.getItem('todo-items');
+        // var items = localStorage.getItem('todo-items');
+        var items = localStorage.getItem(getCurrentDateTime());
         var itemsArr = JSON.parse(items);
 
         for (var i = 0; i < itemsArr.length; i++) {
             var status = ''
             if (itemsArr[i].status == 1) {
                 status = 'class="done"';
-            }
 
-            newItemHtml += `<li data-itemindex="${i}" ${status}>
+                newItemHtml += `<li data-itemindex="${i}" ${status}>
             <span class="item">${itemsArr[i].item}</span>
             <div>
-                <span class="itemComplete">&#10004;</span>
-                <span class="itemDelete">&#10006;</span>
+                <span class="itemComplete"><img src="../images/check-square.svg" alt="ícone tarefa completa" width="16" height="16"></span>
+                <span class="itemEdit"><img src="../images/edit.svg" alt="ícone editar tarefa" width="16" height="16"></span>
+                <span class="itemDelete"><img src="../images/trash.svg" alt="ícone excluir tarefa" width="16" height="16"></span>
             </div>
             </li>`;
+
+            } else {
+                newItemHtml += `<li data-itemindex="${i}" ${status}>
+                <span class="item">${itemsArr[i].item}</span>
+                <div>
+                <span class="itemComplete"><img src="../images/square.svg" alt="ícone tarefa a fazer" width="16" height="16"></span>
+                <span class="itemEdit"><img src="../images/edit.svg" alt="ícone editar tarefa" width="16" height="16"></span>
+                <span class="itemDelete"><img src="../images/trash.svg" alt="ícone excluir tarefa" width="16" height="16"></span>
+                </div>
+                </li>`;
+            }
         }
 
         itemsList.innerHTML = newItemHtml;
@@ -68,6 +86,10 @@ function fetchItems() {
                 var index = this.parentNode.parentNode.dataset.itemindex;
                 itemComplete(index);
             });
+            itemsListUL[i].querySelector('.itemEdit').addEventListener('click', function () {
+                var index = this.parentNode.parentNode.dataset.itemindex;
+                itemEdit(index);
+            });
             itemsListUL[i].querySelector('.itemDelete').addEventListener('click', function () {
                 var index = this.parentNode.parentNode.dataset.itemindex;
                 itemDelete(index);
@@ -76,13 +98,28 @@ function fetchItems() {
         }
 
     } catch (error) {
+        var items = localStorage.getItem('recurring-todo-items');
+        var itemsArr = JSON.parse(items);
+        console.log(items)
+        var dailyItems = [];
 
+        for (var i = 0; i < itemsArr.length; i++) {
+            var value = itemsArr[i].item;
+            console.log(itemsArr[i].item)
+            console.log(itemsArr[i].item.value)
+            dailyItems.push({ "item": value, "status": 0 })
+            const dailyItemsStr = JSON.stringify(dailyItems);
+            console.log(dailyItems)
+            console.log(dailyItemsStr)
+            saveItems(dailyItems)
+        }
+        fetchItems();
     }
 
 }
 
 function itemComplete(index) {
-    var itemsStorage = localStorage.getItem('todo-items');
+    var itemsStorage = localStorage.getItem(getCurrentDateTime());
     var itemsArr = JSON.parse(itemsStorage);
 
     itemsArr[index].status = 1;
@@ -93,8 +130,22 @@ function itemComplete(index) {
 
 }
 
+function itemEdit(index) {
+    console.log('aqui')
+    document.querySelector('.create-task').style.display = 'none';
+    document.querySelector('.new-task').style.display = 'none';
+    document.querySelector('.edit-daily-task-wrapper').style.display = 'block';
+    document.querySelector('.recurring-task-container').style.display = 'none';
+
+    var itemsStorage = localStorage.getItem(getCurrentDateTime());
+    var itemsArr = JSON.parse(itemsStorage);
+
+    document.querySelector('.edit-daily-task input').value = itemsArr[index].item;
+    editingTaskIndex = index;
+}
+
 function itemDelete(index) {
-    var itemsStorage = localStorage.getItem('todo-items');
+    var itemsStorage = localStorage.getItem(getCurrentDateTime());
     var itemsArr = JSON.parse(itemsStorage);
 
     itemsArr.splice(index, 1);
@@ -106,13 +157,26 @@ function itemDelete(index) {
 
 function saveItems(obj) {
     var string = JSON.stringify(obj);
-    localStorage.setItem('todo-items', string);
+    localStorage.setItem(getCurrentDateTime(), string);
 }
 
-document.querySelector('.create-recurring-task').addEventListener('click', function () {
+document.querySelector('.recurring-task-button').addEventListener('click', function () {
+    if (isClosed) {
+        document.querySelector('.recurring-task-container').style.display = 'block';
+        document.querySelector('.recurring-todo-items').style.display = 'block';
+        fetchRecurringItems();
+        isClosed = false;
+    } else {
+        document.querySelector('.recurring-task-container').style.display = 'none';
+        isClosed = true;
+    }
+})
+
+document.querySelector('.create-recurring-task-button').addEventListener('click', function () {
     document.querySelector('.new-recurring-task').style.display = 'block';
-    document.querySelector('.recurring-todo-items').style.display = 'block';
-    fetchRecurringItems();
+    document.querySelector('.create-recurring-task-wrapper').style.display = 'block';
+    document.querySelector('.edit-recurring-task-wrapper').style.display = 'none';
+    document.querySelector('.create-recurring-task-button').style.display = 'none';
 })
 
 document.querySelector('.new-recurring-task button').addEventListener('click', function () {
@@ -123,12 +187,31 @@ document.querySelector('.new-recurring-task button').addEventListener('click', f
         var itemsStorage = localStorage.getItem('recurring-todo-items');
         var itemsArr = JSON.parse(itemsStorage);
 
-        itemsArr.push({"item": itemName });
+        itemsArr.push({ "item": itemName });
 
         saveRecurringItems(itemsArr);
         fetchRecurringItems();
         document.querySelector('.new-recurring-task input').value = '';
         document.querySelector('.new-recurring-task').style.display = 'none';
+        document.querySelector('.create-recurring-task-wrapper').style.display = 'none';
+        document.querySelector('.create-recurring-task-button').style.display = 'block';
+    }
+})
+
+document.querySelector('.edit-recurring-task button').addEventListener('click', function () {
+    var itemName = document.querySelector('.edit-recurring-task input').value;
+
+    if (itemName != '') {
+        var itemsStorage = localStorage.getItem('recurring-todo-items');
+        var itemsArr = JSON.parse(itemsStorage);
+        itemsArr[editingTaskIndex].item = itemName;
+
+        saveRecurringItems(itemsArr);
+        fetchRecurringItems();
+        document.querySelector('.edit-recurring-task input').value = '';
+        document.querySelector('.edit-recurring-task-wrapper').style.display = 'none';
+        // document.querySelector('.create-recurring-task-wrapper').style.display = 'block';
+        document.querySelector('.create-recurring-task-button').style.display = 'block';
     }
 })
 
@@ -147,8 +230,8 @@ function fetchRecurringItems() {
             newItemHtml += `<li data-itemindex="${i}">
             <span class="item">${itemsArr[i].item}</span>
             <div>
-                <span class="recurringItemEdit">&#9998;</span>
-                <span class="recurringItemDelete">&#10006;</span>
+                <span class="recurringItemEdit"><img src="../images/edit.svg" alt="square" width="16" height="16"></span>
+                <span class="recurringItemDelete"><img src="../images/trash.svg" alt="square" width="16" height="16"></span>
             </div>
             </li>`;
         }
@@ -169,23 +252,22 @@ function fetchRecurringItems() {
         }
 
     } catch (error) {
-        localStorage.setItem('recurring-todo-items', '[]');       
+        localStorage.setItem('recurring-todo-items', '[]');
     }
 
 }
 
 function recurringItemEdit(index) {
+    document.querySelector('.create-recurring-task-wrapper').style.display = 'none';
+    document.querySelector('.create-recurring-task-button').style.display = 'none';
+    document.querySelector('.edit-recurring-task').style.display = 'block';
+    document.querySelector('.edit-recurring-task-wrapper').style.display = 'block';
+
     var itemsStorage = localStorage.getItem('recurring-todo-items');
     var itemsArr = JSON.parse(itemsStorage);
 
-    itemsArr[index].item = itemsArr[index].item + '1';
-
-    saveRecurringItems(itemsArr);
-
-    fetchRecurringItems();
-    document.querySelector('.new-recurring-task input').value = '';
-    document.querySelector('.new-recurring-task').style.display = 'none';
-
+    document.querySelector('.edit-recurring-task input').value = itemsArr[index].item;
+    editingTaskIndex = index;
 }
 
 function recurringItemDelete(index) {
@@ -211,11 +293,23 @@ function getCurrentDateTime() {
     const day = String(now.getDate()).padStart(2, '0');
     const hour = String(now.getHours()).padStart(2, '0');
     const minute = String(now.getMinutes()).padStart(2, '0');
+    const min = minute.substring(0, 1)
 
-    return `${year}-${month}-${day}-${hour}-${minute}`;
+    return `${year}-${month}-${day}-${hour}-${min}`;
 }
 
 const currentDateTime = getCurrentDateTime();
+
+function getCurrentDate() {
+    const today = new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = today.toLocaleDateString('pt-BR',
+        options);
+    return formattedDate;
+}
+
+
 console.log(currentDateTime); // Output: 2024-09-08-23-19 (example)
+document.getElementById("currentDate").innerHTML = getCurrentDate();
 
 fetchItems();
